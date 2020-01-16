d4973b14aa70e74a2281a116604a1f2c38e4cb1f
DavideDeFeudis <davidedefeudis@gmail.com>
2020-01-16 17:13:06 +0100

    Finish navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,18 +5,36 @@ import logo from "../logo.svg";
 
 export default function Navbar() {
     return (
-        <nav class="navbar navbar-expand-lg navbar-dark bg-light">
-            <a class="navbar-brand" href="#">Navbar</a>
+        <nav class="navbar navbar-expand-sm navbar-dark bg-light">
+            <Link to="/" className="navbar-brand">
+                Kenan Dinkelmann
+            </Link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-item nav-link active" href="#">Home <span class="sr-only">(current)</span></a>
-                    <a class="nav-item nav-link" href="#">Features</a>
-                    <a class="nav-item nav-link" href="#">Pricing</a>
-                    <a class="nav-item nav-link disabled" href="#">Disabled</a>
-                </div>
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item">
+                        <Link to="/about" className="nav-link">
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/choreography" className="nav-link">
+                            Choreography
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/workshops" className="nav-link">
+                            Workshops
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/contact" className="nav-link">
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
             </div>
         </nav>
     )
