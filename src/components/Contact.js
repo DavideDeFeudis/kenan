4778d138321706f573c2937b1c9d6f0557917b0d
DavideDeFeudis <davidedefeudis@gmail.com>
2020-01-27 14:02:21 +0100

    Form.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,56 +1,15 @@
 import React from 'react'
-import { useState } from 'react';
 import '../index.css';
 import backgroundLarge from "../images/contact_1920.jpg";
 // import backgroundSmall from "../images/contact_500.jpg";
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Background from './Background';
-import { ButtonContainer } from './Button'
+// import { Consumer } from "../context";
+import Form from './Form';
 
 export default function Contact() {
-
-    const [message, setMessage] = useState({
-        name: '',
-        email: '',
-        subject: '',
-        text: ''
-    })
-
-    const [feedback, setFeedback] = useState('')
-
-    const handleChange = (e) => {
-        setMessage({ ...message, [e.target.name]: e.target.value })
-        // remove feedback when typing a second message
-        if (feedback) setFeedback('')
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        sendFormData()
-        console.log(message)
-    }
-
-    const sendFormData = () => {
-        fetch('http://localhost:4000/contact', {
-            method: "POST",
-            body: JSON.stringify(message),
-            headers: {
-                "Content-Type": "application/json"
-            }
-        })
-            .then(response => response.json())
-            .then(json => {
-                setFeedback(json.message)
-                setMessage({
-                    name: '',
-                    email: '',
-                    subject: '',
-                    text: ''
-                })
-                console.log(json)
-            })
-    }
+    // const { handleChange, handleSubmit } = useContext(Consumer)
 
     return (
         <div className='Contact'>
@@ -59,67 +18,7 @@ export default function Contact() {
                 // small={backgroundSmall}
                 large={backgroundLarge}
             />
-            <div className="container">
-                <form
-                    action=""
-                    onSubmit={handleSubmit}
-                >
-                    <div className="form-group">
-                        <input
-                            name="name"
-                            type="text"
-                            className="form-control"
-                            placeholder="Name"
-                            onChange={handleChange}
-                            value={message.name}
-                        >
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <input
-                            name="email"
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                            onChange={handleChange}
-                            value={message.email}
-                            required
-                        >
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <input
-                            name="subject"
-                            type="text"
-                            className="form-control"
-                            placeholder="Subject"
-                            onChange={handleChange}
-                            value={message.subject}
-                        >
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <textarea
-                            name="text"
-                            className="form-control"
-                            rows="3"
-                            placeholder="Message"
-                            onChange={handleChange}
-                            value={message.text}
-                            required
-                        >
-                        </textarea>
-                    </div>
-                    {/* <button type="submit" className="btn btn-secondary mb-2">Send</button> */}
-                    <ButtonContainer
-                        type="submit"
-                        className="mb-2"
-                    >
-                        Send
-                    </ButtonContainer>
-                    <p>{feedback}</p>
-                </form>
-            </div>
+            <Form />
             <Footer />
         </div>
     )
---
+++ src/components/Form.js
@@ -0,0 +1,126 @@
+import React, { useState } from 'react'
+import styled from "styled-components";
+import { ButtonContainer } from './Button'
+
+export default function Form() {
+    const [message, setMessage] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        text: ''
+    })
+
+    const [feedback, setFeedback] = useState('')
+
+    const handleChange = (e) => {
+        setMessage({ ...message, [e.target.name]: e.target.value })
+        if (feedback) setFeedback('')
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        sendFormData()
+        console.log(message)
+    }
+
+    const sendFormData = () => {
+        fetch('http://localhost:4000/contact', {
+            method: "POST",
+            body: JSON.stringify(message),
+            headers: {
+                "Content-Type": "application/json"
+            }
+        })
+            .then(response => response.json())
+            .then(json => {
+                setFeedback(json.message)
+                setMessage({
+                    name: '',
+                    email: '',
+                    subject: '',
+                    text: ''
+                })
+                console.log(json)
+            })
+    }
+
+    return (
+        <FormContainer>
+            <div className="container">
+                <form
+                    action=""
+                    onSubmit={handleSubmit}
+                >
+                    <div className="form-group">
+                        <input
+                            name="name"
+                            type="text"
+                            className="form-control"
+                            placeholder="Name"
+                            onChange={handleChange}
+                            value={message.name}
+                        >
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            name="email"
+                            type="email"
+                            className="form-control"
+                            placeholder="Email"
+                            onChange={handleChange}
+                            value={message.email}
+                            required
+                        >
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <input
+                            name="subject"
+                            type="text"
+                            className="form-control"
+                            placeholder="Subject"
+                            onChange={handleChange}
+                            value={message.subject}
+                        >
+                        </input>
+                    </div>
+                    <div className="form-group">
+                        <textarea
+                            name="text"
+                            className="form-control"
+                            rows="3"
+                            placeholder="Message"
+                            onChange={handleChange}
+                            value={message.text}
+                            required
+                        >
+                        </textarea>
+                    </div>
+                    <ButtonContainer
+                        type="submit"
+                        className="mb-2"
+                    >
+                        Send
+                    </ButtonContainer>
+                    <p>{feedback}</p>
+                </form>
+            </div>
+        </FormContainer>
+    )
+}
+
+const FormContainer = styled.div`
+    margin-top: 5vh;
+    margin-bottom: 5vh;
+    text-align: center; 
+  
+    textarea, 
+    textarea:focus, 
+    input, 
+    input:focus {
+        background-color: rgb(21, 21, 21);
+        border-color: rgb(55, 55, 55);
+        color: #FFF;
+    }
+`;
