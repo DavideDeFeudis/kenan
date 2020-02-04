6dda187af3bb5b153f9303d2f8582af2a7171a66
DavideDeFeudis <davidedefeudis@gmail.com>
2020-02-04 12:11:22 +0100

    Refactor style workshops

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import { SocialIcon } from 'react-social-icons';
 
 export default function Footer() {
     return (
-        <footer className="text-center py-4 mt-3">
+        <footer className="text-center py-4">
             <SocialIcon className="mx-1" url="http://instagram.com" bgColor="#919191" style={{ height: 40, width: 40 }}/>
             <SocialIcon className="mx-1" url="https://www.youtube.com/channel/UCHUoc2LX0yZAGkzrfoGoCMA" bgColor="#919191" style={{ height: 40, width: 40 }}/>
             <SocialIcon className="mx-1" url="https://vimeo.com/user10326182" bgColor="#919191" style={{ height: 40, width: 40 }}/>
--- src/components/Workshops.js
+++ src/components/Workshops.js
@@ -20,12 +20,14 @@ export default function Workshops() {
                 small={backgroundSmall}
                 large={backgroundLarge}
             />
-            <div className="container main-content text-center">
-                <p className="main-paragraph pb-5">
+            <div className="container main-content text-center py-5">
+                <p className="main-paragraph">
                     Drawing inspirations from martial arts and yoga, the warm up has the purpose to tune the dancers into a clear and subtle state of body and mind before their practice and execution. Challenging postures alongside movement flow with circular motions and spirals will be introduced in order to provide better awareness of the body, especially for wrists and legs.<br/><br/>
                     The warm up is followed by a series of movement patterns used as a tool to explore the mechanics of the spine. Introducing dynamics of balancing, stabilizing, falling, rolling, flipping and melting down in order to maintain flow without unnecessary tension. The patterns will increase in complexity and then be linked into longer sequences with acrobatic elements.<br/><br/>
                     The main part of the seminar consists of high energy movement phrases based on Kenans choreographic work which is influenced by various backgrounds such as Capoeira, Judo, Release-Technic and Breakdance. Improvisation tasks, either alone or with a partner will be present during the whole workshop in order to explore the movement material from another viewpoint.
                 </p>
+            </div>
+            <div className="container workshops-list text-center">
                 {
                     workshops.map(workshop => {
                         return <Workshop
