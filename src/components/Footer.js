aa12a8c2e07e0f2b92afe206adb273a9b9c37019
DavideDeFeudis <davidedefeudis@gmail.com>
2020-02-16 14:12:04 +0100

    Update navbar and footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,13 @@ import React from 'react'
 import '../styles/index.scss';
 import { SocialIcon } from 'react-social-icons';
 
-
 export default function Footer() {
     return (
         <footer className="text-center py-4">
-            <SocialIcon className="mx-1" url="http://instagram.com" bgColor="#919191" style={{ height: 40, width: 40 }}/>
-            <SocialIcon className="mx-1" url="https://www.youtube.com/channel/UCHUoc2LX0yZAGkzrfoGoCMA" bgColor="#919191" style={{ height: 40, width: 40 }}/>
-            <SocialIcon className="mx-1" url="https://vimeo.com/user10326182" bgColor="#919191" style={{ height: 40, width: 40 }}/>
+            <SocialIcon target="_blank" rel="noopener noreferrer" className="social-icon mx-1" url="https://www.instagram.com/kenandinkelmann/" bgColor="#919191" style={{ height: 40, width: 40 }}/>
+            <SocialIcon target="_blank" rel="noopener noreferrer" className="social-icon mx-1" url="https://web.facebook.com/kenanchoreography" bgColor="#919191" style={{ height: 40, width: 40 }}/>
+            <SocialIcon target="_blank" rel="noopener noreferrer" className="social-icon mx-1" url="https://www.youtube.com/channel/UCHUoc2LX0yZAGkzrfoGoCMA" bgColor="#919191" style={{ height: 40, width: 40 }}/>
+            <SocialIcon target="_blank" rel="noopener noreferrer" className="social-icon mx-1" url="https://vimeo.com/user10326182" bgColor="#919191" style={{ height: 40, width: 40 }}/>
         </footer>
     )
 }
