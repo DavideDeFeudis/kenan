df1f28458aae6491a2cc5e6b980185b8ad6f81c7
DavideDeFeudis <davidedefeudis@gmail.com>
2020-02-17 23:48:58 +0100

    Login.js, SignUp.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Admin from "./components/Admin";
-import AdminLogin from './components/AdminLogin';
+// import AdminLogin from './components/AdminLogin';
 import About from "./components/About";
 import VideoAstral from "./components/VideoAstral";
 import VideoSunson from "./components/VideoSunson";
 // import VideoSection from "./components/VideoSection";
 import Workshops from "./components/Workshops";
 import Contact from "./components/Contact";
+import PrivateRoute from "./PrivateRoute";
+import SignUp from "./components/SignUp";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -16,7 +19,9 @@ function App() {
       <Switch>
         <PrivateRoute exact path="/admin" component={Admin} />
         {/* <Route exact path="/admin" component={Admin} /> */}
-        <Route path="/admin/login" component={AdminLogin} />
+        {/* <Route path="/admin/login" component={AdminLogin} /> */}
+        <Route exact path="/signup" component={SignUp} />
+        <Route exact path="/login" component={Login} />
         <Route path="/about" component={About} />
         {/* <Route path="/videoSection" component={VideoSection} /> */}
         <Route path="/astral" component={VideoAstral} />
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,118 +1,48 @@
-import React, { useState } from 'react'
-import styled from "styled-components";
-import { Button } from './Button'
+import React, { useCallback, useContext } from "react";
+import { withRouter, Redirect } from "react-router";
+import app from "../base.js";
+import { AuthContext } from "../Auth.js";
 
-export default function Login() {
-    const [formData, setFormData] = useState({
-        name: '',
-        email: '',
-        password: ''
-    })
-
-    const [feedback, setFeedback] = useState('')
-
-    const handleChange = (e) => {
-        setFormData({ ...formData, [e.target.name]: e.target.value })
-        if (feedback) setFeedback('')
-    }
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        sendFormData()
-        console.log('formData:', formData)
-    }
-
-    const sendFormData = () => {
-        fetch(`${process.env.REACT_APP_BACKEND_HOST}/login`, {
-            method: "POST",
-            body: JSON.stringify(formData),
-            headers: {
-                "Content-Type": "application/json"
+const Login = ({ history }) => {
+    const handleLogin = useCallback(
+        async event => {
+            event.preventDefault();
+            const { email, password } = event.target.elements;
+            try {
+                await app
+                    .auth()
+                    .signInWithEmailAndPassword(email.value, password.value);
+                history.push("/admin");
+            } catch (error) {
+                console.log('admin login error:', error)
+                alert(error);
             }
-        })
-            .then(response => response.json())
-            .then(json => {
-                setFeedback(json.message)
-                setFormData({
-                    name: '',
-                    email: '',
-                    password: ''
-                })
-                console.log('json:', json)
-            })
-            .catch(err => {
-                setFeedback('Error sending form data to the server.')
-                console.log('err:', err)
-            })
+        },
+        [history]
+    );
+
+    const { currentUser } = useContext(AuthContext);
+
+    if (currentUser) {
+        return <Redirect to="/admin" />;
     }
 
     return (
-        <FormContainer>
-            <div className="container">
-                <h1>Login to the admin area</h1>
-                <form
-                    onSubmit={handleSubmit}
-                >
-                    <div className="form-group">
-                        <input
-                            name="name"
-                            type="text"
-                            className="form-control"
-                            placeholder="Name"
-                            onChange={handleChange}
-                            value={formData.name}
-                            required
-                        >
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <input
-                            name="email"
-                            type="email"
-                            className="form-control"
-                            placeholder="Email"
-                            onChange={handleChange}
-                            value={formData.email}
-                            required
-                        >
-                        </input>
-                    </div>
-                    <div className="form-group">
-                        <input
-                            name="password"
-                            type="password"
-                            className="form-control"
-                            placeholder="Password"
-                            onChange={handleChange}
-                            value={formData.password}
-                            required
-                        >
-                        </input>
-                    </div>
-                    <Button
-                        type="submit"
-                        className="mb-2"
-                    >
-                        Login
-                    </Button>
-                    <p>{feedback}</p>
-                </form>
-            </div>
-        </FormContainer>
-    )
-}
+        <div>
+            <h1>Log in</h1>
+            <form onSubmit={handleLogin}>
+                <label>
+                    Email
+                <input name="email" type="email" placeholder="Email" />
+                </label>
+                <label>
+                    Password
+                <input name="password" type="password" placeholder="Password" />
+                </label>
+                <button type="submit">Log in</button>
+            </form>
+        </div>
+    );
+};
 
-const FormContainer = styled.div`
-    margin-top: 5vh;
-    // margin-bottom: 5vh;
-    text-align: center; 
-  
-    textarea, 
-    textarea:focus, 
-    input, 
-    input:focus {
-        background-color: rgb(21, 21, 21);
-        border-color: rgb(55, 55, 55);
-        color: #FFF;
-    }
-`;
+export default withRouter(Login);
