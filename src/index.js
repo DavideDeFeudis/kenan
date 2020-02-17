2d7c02c63491fe46cfcc2a9baaec87e5c97df02c
DavideDeFeudis <davidedefeudis@gmail.com>
2020-02-17 21:04:20 +0100

    AuthProvider

--- src/index.js
+++ src/index.js
@@ -8,13 +8,16 @@ import App from './App';
 import { BrowserRouter as Router } from "react-router-dom";
 import * as serviceWorker from './serviceWorker';
 import { Provider } from './context';
+import { AuthProvider } from "./Auth";
 
 ReactDOM.render(
-    <Provider>
-        <Router>
-            <App />
-        </Router>
-    </Provider>,
+    <AuthProvider>
+        <Provider>
+            <Router>
+                <App />
+            </Router>
+        </Provider>
+    </AuthProvider>,
     document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
