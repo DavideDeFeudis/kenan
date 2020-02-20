f37a88edf8588c14babcf283661cf16fc9b1e441
DavideDeFeudis <davidedefeudis@gmail.com>
2020-02-20 12:19:51 +0100

    Home: full screen image, delete text

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/index.scss';
-import backgroundLarge from "../images/home_1920.jpg";
+import backgroundLarge from "../images/home_full_1920.jpg";
 import backgroundSmall from "../images/home_500.jpg";
 import Navbar from './Navbar';
 import Footer from './Footer';
@@ -15,11 +15,6 @@ export default function Home() {
                 small={backgroundSmall}
                 large={backgroundLarge}
             />
-            <div className="container main-content text-center">
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. A nisi dolore rem amet aut magnam voluptates excepturi maiores soluta perferendis beatae expedita vero esse voluptate inventore commodi, quisquam, ex necessitatibus voluptas eos. Incidunt quos soluta dignissimos eveniet esse. Aperiam accusantium molestiae ipsa porro repellendus laudantium adipisci quasi laborum incidunt sed.
-                </p>
-            </div>
             <Footer />
         </div>
     )
