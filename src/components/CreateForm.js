7b0be8cd6c573f76b034d5d05dff748eceaee40e
DavideDeFeudis <davidedefeudis@gmail.com>
2020-03-23 20:46:04 +0100

    Re-design admin: inputs on 1/2/4 col, published at bottom

--- src/components/CreateForm.js
+++ src/components/CreateForm.js
@@ -1,121 +1,144 @@
-import React from 'react'
-import { Button } from './Button'
-import Input from './form/Input'
-import { createWorkshop } from '../databaseService'
+import React from "react";
+import { Button } from "./Button";
+import Input from "./form/Input";
+import { createWorkshop } from "../databaseService";
 
 export default function CreateForm(props) {
-    const { newWorkshop, setNewWorkshop, addWorkshopToTempWS, clearInputs } = props
-    // console.log('CreateForm props:', props)
-    const inputNamesCol1 = ['title']
-    // const inputNamesCol1 = ['title', 'date']
-    const inputNamesCol2 = ['address', 'info']
-    const inputNamesCol3 = ['priceLabel1', 'priceLabel2', 'priceLabel3', 'priceLabel4']
-    const inputNamesCol4 = ['price1', 'price2', 'price3', 'price4']
+  const {
+    newWorkshop,
+    setNewWorkshop,
+    addWorkshopToTempWS,
+    clearInputs
+  } = props;
+  const inputNamesCol1 = ["title", "address", "info"];
+  //   const inputNamesCol2 = ["address", "info"];
+  const inputNamesCol3 = [
+    "priceLabel1",
+    "priceLabel2",
+    "priceLabel3",
+    "priceLabel4"
+  ];
+  const inputNamesCol4 = ["price1", "price2", "price3", "price4"];
 
-    const handleChange = (e) => {
-        setNewWorkshop({
-            ...newWorkshop,
-            [e.target.name]: e.target.value
-        })
-    }
+  const handleChange = e => {
+    setNewWorkshop({
+      ...newWorkshop,
+      [e.target.name]: e.target.value
+    });
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        addWorkshopToTempWS()
-        createWorkshop(newWorkshop)
-    }
+  const handleSubmit = e => {
+    e.preventDefault();
+    addWorkshopToTempWS();
+    createWorkshop(newWorkshop);
+  };
 
-    return (
-        <form className="form create-form pt-3" onSubmit={handleSubmit}>
-            <div className="form-row">
-                <div className="col-sm-6">
-                    <label htmlFor="start-date">Starting date</label>
-                    <Input
-                        id='start-date'
-                        type='date'
-                        name='startDate'
-                        onChange={handleChange}
-                        newWorkshop={newWorkshop}
-                    />
-                    <label htmlFor="start-time">Starting time</label>
-                    <Input
-                        id='start-time'
-                        type='time'
-                        name='startTime'
-                        onChange={handleChange}
-                        newWorkshop={newWorkshop}
-                    />
-                    {
-                        inputNamesCol1.map((inputName, i) => (
-                            <Input
-                                key={i}
-                                name={inputName}
-                                onChange={handleChange}
-                                newWorkshop={newWorkshop}
-                            />
-                        ))
-                    }
-                </div>
-                <div className="col-sm-6">
-                    <label htmlFor="end-date">Ending date</label>
-                    <Input
-                        id='end-date'
-                        type='date'
-                        name='endDate'
-                        onChange={handleChange}
-                        newWorkshop={newWorkshop}
-                    />
-                    <label htmlFor="end-time">Ending time</label>
-                    <Input
-                        id='end-time'
-                        type='time'
-                        name='endTime'
-                        onChange={handleChange}
-                        newWorkshop={newWorkshop}
-                    />
-                    {
-                        inputNamesCol2.map((inputName, i) => (
-                            <Input
-                                key={i}
-                                name={inputName}
-                                onChange={handleChange}
-                                newWorkshop={newWorkshop}
-                            />
-                        ))
-                    }
-                </div>
-            </div>
-            <div className="form-row">
-                <div className="col-sm-6">
-                    {
-                        inputNamesCol3.map((inputName, i) => (
-                            <Input
-                                key={i}
-                                name={inputName}
-                                onChange={handleChange}
-                                newWorkshop={newWorkshop}
-                            />
-                        ))
-                    }
-                </div>
-                <div className="col-sm-6">
-                    {
-                        inputNamesCol4.map((inputName, i) => (
-                            <Input
-                                key={i}
-                                name={inputName}
-                                type="number"
-                                onChange={handleChange}
-                                newWorkshop={newWorkshop}
-                            />
-                        ))
-                    }
-                </div>
-            </div>
-            <div className="my-4">
-                <Button className='admin-button' type="submit">Publish</Button>
-                <Button onClick={clearInputs} className='admin-button' type="button">Clear Inputs</Button>
-            </div>
-        </form>
-    )
+  return (
+    <form className="form create-form pt-3" onSubmit={handleSubmit}>
+      <div className="form-row">
+        <div className="col-sm-6 col-lg">
+          {inputNamesCol1.map((inputName, i) => (
+            <>
+              <label htmlFor={inputName}>{inputName}</label>
+              <Input
+                key={i}
+                id={inputName}
+                name={inputName}
+                onChange={handleChange}
+                newWorkshop={newWorkshop}
+              />
+            </>
+          ))}
+
+          {/* {inputNamesCol2.map((inputName, i) => (
+            <>
+              <label htmlFor={inputName}>{inputName}</label>
+              <Input
+                key={i}
+                id={inputName}
+                name={inputName}
+                onChange={handleChange}
+                newWorkshop={newWorkshop}
+              />
+            </>
+          ))} */}
+        </div>
+
+        <div className="col-sm-6 col-lg">
+          <label htmlFor="start-date">Starting date</label>
+          <Input
+            id="start-date"
+            type="date"
+            name="startDate"
+            onChange={handleChange}
+            newWorkshop={newWorkshop}
+          />
+          <label htmlFor="start-time">Starting time</label>
+          <Input
+            id="start-time"
+            type="time"
+            name="startTime"
+            onChange={handleChange}
+            newWorkshop={newWorkshop}
+          />
+
+          <label htmlFor="end-date">Ending date</label>
+          <Input
+            id="end-date"
+            type="date"
+            name="endDate"
+            onChange={handleChange}
+            newWorkshop={newWorkshop}
+          />
+          <label htmlFor="end-time">Ending time</label>
+          <Input
+            id="end-time"
+            type="time"
+            name="endTime"
+            onChange={handleChange}
+            newWorkshop={newWorkshop}
+          />
+        </div>
+
+        <div className="col-sm-6 col-lg">
+          {inputNamesCol3.map((inputName, i) => (
+            <>
+              <label htmlFor={inputName}>{inputName}</label>
+              <Input
+                key={i}
+                id={inputName}
+                name={inputName}
+                onChange={handleChange}
+                newWorkshop={newWorkshop}
+              />
+            </>
+          ))}
+        </div>
+        <div className="col-sm-6 col-lg">
+          {inputNamesCol4.map((inputName, i) => (
+            <>
+              <label htmlFor={inputName}>{inputName}</label>
+              <Input
+                key={i}
+                id={inputName}
+                name={inputName}
+                type="number"
+                onChange={handleChange}
+                newWorkshop={newWorkshop}
+              />
+            </>
+          ))}
+        </div>
+      </div>
+
+      <div className="my-4">
+        <Button className="admin-button" type="submit">
+          Publish
+        </Button>
+        <Button onClick={clearInputs} className="admin-button" type="button">
+          Clear Inputs
+        </Button>
+      </div>
+    </form>
+  );
 }
--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,22 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Input(props) {
-    let { name, type, placeholder, onChange, required, newWorkshop } = props
-    if (!type) type = "text"
-    if (!placeholder) placeholder = name.charAt(0).toUpperCase() + name.slice(1)
+  let { name, type, placeholder, onChange, required, newWorkshop } = props;
+  if (!type) type = "text";
+  // if (!placeholder) placeholder = name.charAt(0).toUpperCase() + name.slice(1)
 
-    return (
-        <input
-            className="form-control"
-            name={name}
-            type={type}
-            placeholder={placeholder}
-            onChange={onChange}
-            value={newWorkshop[name] || ''} // || '' avoids Warning: `value` prop on `input` should not be null. Consider using an empty string to clear the component
-            // required={required}
-            required
-            autoComplete='off'
-        >
-        </input>
-    )
+  return (
+    <input
+      className="form-control mb-4"
+      name={name}
+      type={type}
+      // placeholder={placeholder}
+      onChange={onChange}
+      value={newWorkshop[name] || ""} // || '' avoids Warning: `value` prop on `input` should not be null. Consider using an empty string to clear the component
+      // required={required}
+      required
+      autoComplete="off"
+    ></input>
+  );
 }
