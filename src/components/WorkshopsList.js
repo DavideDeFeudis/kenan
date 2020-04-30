0ed52906349c53e184dd0019c3640af97125c82a
DavideDeFeudis <davidedefeudis@gmail.com>
2020-04-30 08:32:02 +0200

    Use local ws

--- src/components/WorkshopsList.js
+++ src/components/WorkshopsList.js
@@ -1,42 +1,43 @@
-import React from 'react'
-import Workshop from './Workshop'
-import loadingGif from '../images/load.gif'
-import { useHttp } from '../hooks/http'
+import React from "react";
+import Workshop from "./Workshop";
+import loadingGif from "../images/load.gif";
+// import { useHttp } from '../hooks/http'
+import { localWorkshops } from "../data";
 
 export default function WorkshopsList() {
-    const url = process.env.REACT_APP_BACKEND_HOST + '/workshops'
-    const [isLoading, fetchedData] = useHttp(url, [])
-    let content = (
-        <div className="fetching-spinner container text-center">
-            <img src={loadingGif} width='40' height='40' alt="loading..." />
-            <p className='mt-3'>Loading workshops...</p>
-        </div>
-    )
-    const workshopsList = (
-        fetchedData ? fetchedData.sort((a, b) => { // need check fetchedData because it is null on first render because useEffect within useHttp runs after the first render
-            const aDate = parseInt(a.startDate.split('-').join(''))
-            const bDate = parseInt(b.startDate.split('-').join(''))
-            return aDate - bDate
-        }).map(workshop => {
-            return <Workshop
-                user
-                key={workshop.secondaryID}
-                workshop={workshop}
-            />
-        }) : []
-    )
-    if (!isLoading && fetchedData && fetchedData.length > 0) {
-        content = workshopsList
-        // } else if (!isLoading && (!fetchedData || fetchedData.length === 0)) {
-        //     content = <p className='my-5'>There are no workshops at the moment</p>
-    } else if (!isLoading && !fetchedData) {
-        content = <p className='my-5'>There was a problem loading workshops</p>
-    } else if (!isLoading && fetchedData.length === 0) {
-        content = <p className='my-5'>There are no workshops at the moment</p>
-    }
-    return (
-        <div className="container text-center">
-            {content}
-        </div>
-    )
+  // const url = process.env.REACT_APP_BACKEND_HOST + '/workshops'
+  // const [isLoading, fetchedData] = useHttp(url, [])
+  const isLoading = false;
+  const fetchedData = localWorkshops;
+
+  let content = (
+    <div className="fetching-spinner container text-center">
+      <img src={loadingGif} width="40" height="40" alt="loading..." />
+      <p className="mt-3">Loading workshops...</p>
+    </div>
+  );
+  const workshopsList = fetchedData
+    ? fetchedData
+        .sort((a, b) => {
+          // need check fetchedData because it is null on first render because useEffect within useHttp runs after the first render
+          const aDate = parseInt(a.startDate.split("-").join(""));
+          const bDate = parseInt(b.startDate.split("-").join(""));
+          return aDate - bDate;
+        })
+        .map((workshop) => {
+          return (
+            <Workshop user key={workshop.secondaryID} workshop={workshop} />
+          );
+        })
+    : [];
+  if (!isLoading && fetchedData && fetchedData.length > 0) {
+    content = workshopsList;
+    // } else if (!isLoading && (!fetchedData || fetchedData.length === 0)) {
+    //     content = <p className='my-5'>There are no workshops at the moment</p>
+  } else if (!isLoading && !fetchedData) {
+    content = <p className="my-5">There was a problem loading workshops</p>;
+  } else if (!isLoading && fetchedData.length === 0) {
+    content = <p className="my-5">There are no workshops at the moment</p>;
+  }
+  return <div className="container text-center">{content}</div>;
 }
--- src/data.js
+++ src/data.js
@@ -1,41 +1,56 @@
 export const localWorkshops = [
-    {
-        _id: 1,
-        secondaryID: 11,
-        title: 'Flow Acrobatics - Dresden',
-        date: '18-19.04.2020',
-        address: 'Bautzner Straße 107',
-        info: 'For all levels',
-        priceLabel1: 'One day: €',
-        priceLabel2: 'Both days: €',
-        price1: 60,
-        price2: 95
-    },
-    {
-        _id: 2,
-        secondaryID: 22,
-        title: 'Flow Acrobatics - Hamburg',
-        date: '25-26.04.2020',
-        address: 'Valentinskamp 34A',
-        info: 'For all levels',
-        priceLabel1: 'One day: €',
-        priceLabel2: 'Both days: €',
-        price1: 60,
-        price2: 95
-    }
-]
+  {
+    _id: 1,
+    secondaryID: 11,
+    title: "Flow Acrobatics - Dresden",
+    address: "Bautzner Straße 107",
+    info: "For all levels",
+    startDate: "2020-06-25",
+    endDate: "2020-06-26",
+    priceLabel1: "One day: €",
+    priceLabel2: "Both days: €",
+    price1: 60,
+    price2: 95,
+  },
+  //   {
+  //     _id: 2,
+  //     secondaryID: 22,
+  //     title: "Flow Acrobatics - Hamburg",
+  //     date: "25-26.07.2020",
+  //     address: "Valentinskamp 34A",
+  //     info: "For all levels",
+  //     priceLabel1: "One day: €",
+  //     priceLabel2: "Both days: €",
+  //     price1: 60,
+  //     price2: 95,
+  //   },
+  //   {
+  //     _id: 3,
+  //     secondaryID: 33,
+  //     title: "Flow Acrobatics - Barcelona",
+  //     date: "25-26.08.2020",
+  //     address: "Calle S. Juan 3",
+  //     info: "For all levels",
+  //     priceLabel1: "One day: €",
+  //     priceLabel2: "Both days: €",
+  //     price1: 60,
+  //     price2: 95,
+  //   },
+];
 
 export const localVideos = [
-    {
-        id: 1,
-        title: 'Astral',
-        url: 'https://www.youtube.com/watch?v=dYEsi-ORb44',
-        info: 'ASTRAL is set in a fictional world where humankind, through its behavior, has made survival on planet Earth impossible. Climate change has progressed so drastically that no human being can survive on Earth in 10 years. Scientists travel to Mars to build up a new society. How do they react to suddenly being pulled out of their normal living environment? Humanity is forced to question past patterns of behavior and create new concepts to secure the long-term future of society on a new planet. How does the individual deal with severe changes on a psychological level? ASTRAL is an attempt to display the process of dealing with changes with and within the body. Direction and choreography: Kenan Dinkelmann. Performers: Nitzan Moshe, Miriam Kaya, Joshua Smith, Max Makowski. Supported by Derida Dance Centre, Sofia, Bulgaria'
-    },
-    {
-        id: 2,
-        title: 'Sunson',
-        url: 'https://www.youtube.com/watch?v=FenzrFVlvx4',
-        info: 'SUNSON description. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab maxime animi porro doloribus quod assumenda recusandae, reprehenderit sit voluptatibus sequi repudiandae soluta voluptatem consequuntur doloremque nulla ad distinctio earum alias modi ullam repellat? Optio quae laudantium aperiam atque consequatur aut?'
-    }
-]
+  {
+    id: 1,
+    title: "Astral",
+    url: "https://www.youtube.com/watch?v=dYEsi-ORb44",
+    info:
+      "ASTRAL is set in a fictional world where humankind, through its behavior, has made survival on planet Earth impossible. Climate change has progressed so drastically that no human being can survive on Earth in 10 years. Scientists travel to Mars to build up a new society. How do they react to suddenly being pulled out of their normal living environment? Humanity is forced to question past patterns of behavior and create new concepts to secure the long-term future of society on a new planet. How does the individual deal with severe changes on a psychological level? ASTRAL is an attempt to display the process of dealing with changes with and within the body. Direction and choreography: Kenan Dinkelmann. Performers: Nitzan Moshe, Miriam Kaya, Joshua Smith, Max Makowski. Supported by Derida Dance Centre, Sofia, Bulgaria",
+  },
+  {
+    id: 2,
+    title: "Sunson",
+    url: "https://www.youtube.com/watch?v=FenzrFVlvx4",
+    info:
+      "SUNSON description. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab maxime animi porro doloribus quod assumenda recusandae, reprehenderit sit voluptatibus sequi repudiandae soluta voluptatem consequuntur doloremque nulla ad distinctio earum alias modi ullam repellat? Optio quae laudantium aperiam atque consequatur aut?",
+  },
+];
