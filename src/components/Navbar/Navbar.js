fe891f37dd3de7e127415002d3319c04517052ad
DavideDeFeudis <davidedefeudis@gmail.com>
2020-08-15 14:05:44 +0200

    CSS module navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -8,42 +8,20 @@ export default function Navbar({ admin }) {
   const { videos } = useContext(StateContext);
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark bg-light">
-      <Link to="/" className="navbar-brand">
-        Kenan Dinkelmann
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item">
-            <Link to="/about" className="nav-link">
-              About
-            </Link>
-          </li>
-          <li className="nav-item dropdown">
-            <Link
-              className="nav-link dropdown-toggle"
-              to="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-              Choreography
-            </Link>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-              {videos.map((video) => (
+    <nav className={classes.menu}>
+      <ol>
+        <li className={classes.menuItem}>
+          <Link to="/">Home</Link>
+        </li>
+        <li className={classes.menuItem}>
+          <Link to="/about">About</Link>
+        </li>
+        <li className={classes.menuItem}>
+          <Link to="/choreography">Choreography</Link>
+
+          <ol className={classes.subMenu}>
+            {videos.map((video) => (
+              <li className={classes.menuItem}>
                 <Link
                   key={video.id}
                   url={video.url}
@@ -53,38 +31,31 @@ export default function Navbar({ admin }) {
                 >
                   {video.title}
                 </Link>
-              ))}
-            </div>
-          </li>
-          <li className="nav-item">
-            <Link to="/online-coaching" className="nav-link">
-              Online Coaching
+              </li>
+            ))}
+          </ol>
+        </li>
+        <li className={classes.menuItem}>
+          <Link to="/online-coaching">Online Coaching</Link>
+        </li>
+        <li className={classes.menuItem}>
+          <Link to="/workshops">Workshops</Link>
+        </li>
+        <li className={classes.menuItem}>
+          <Link to="/contact">Contact</Link>
+        </li>
+        {admin && (
+          <li className={classes.menuItem}>
+            <Link
+              to="/login"
+              data-test="admin-nav-link"
+              onClick={() => app.auth().signOut()}
+            >
+              Sign Out
             </Link>
           </li>
-          <li className="nav-item">
-            <Link to="/workshops" className="nav-link">
-              Workshops
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="/contact" className="nav-link">
-              Contact
-            </Link>
-          </li>
-          {admin && (
-            <li className="nav-item">
-              <Link
-                to="/login"
-                data-test="admin-nav-link"
-                className="nav-link"
-                onClick={() => app.auth().signOut()}
-              >
-                Sign Out
-              </Link>
-            </li>
-          )}
-        </ul>
-      </div>
+        )}
+      </ol>
     </nav>
   );
 }
