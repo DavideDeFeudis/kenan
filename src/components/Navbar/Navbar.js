0872bcdd9e5f65678f93ce76046f1a3f68cbd05c
DavideDeFeudis <davidedefeudis@gmail.com>
2021-07-13 17:21:40 +0200

    Navbar: remove unused links

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,36 +1,14 @@
-import React, { useContext } from "react";
+import React from "react";
 import classes from "./Navbar.module.css";
 import { Link } from "react-router-dom";
-// import { StateContext } from "../../context";
-import app from "../../base";
 
-export default function Navbar({ admin }) {
-    // const { videos } = useContext(StateContext);
-
+export default function Navbar() {
     return (
         <nav className={classes.menu}>
             <ol>
                 <li className={classes.menuItem}>
                     <Link to="/">Home</Link>
                 </li>
-                {/* <li className={classes.menuItem}>
-                    <Link to="/about">About</Link>
-                </li>
-                <li className={classes.menuItem}>
-                    <Link to="/choreography">Choreography</Link>
-                    <ol className={classes.subMenu}>
-                        {videos.map((video) => (
-                            <li className={classes.menuItem} key={video.id}>
-                                <Link url={video.url} info={video.info} className="dropdown-item" to={"/" + video.title.toLowerCase()}>
-                                    {video.title}
-                                </Link>
-                            </li>
-                        ))}
-                    </ol>
-                </li>
-                <li className={classes.menuItem}>
-                    <Link to="/online-coaching">Online Coaching</Link>
-                </li> */}
                 <li className={classes.menuItem}>
                     <Link to="/workshops">Workshops</Link>
                 </li>
@@ -40,13 +18,6 @@ export default function Navbar({ admin }) {
                 <li className={classes.menuItem}>
                     <Link to="/admin">Admin</Link>
                 </li>
-                {/* {admin && (
-                    <li className={classes.menuItem}>
-                        <Link to="/login" data-test="admin-nav-link" onClick={() => app.auth().signOut()}>
-                            Sign Out
-                        </Link>
-                    </li>
-                )} */}
             </ol>
         </nav>
     );
