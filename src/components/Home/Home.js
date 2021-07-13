2437da2c423c820d135456757f04e1c4063abbeb
DavideDeFeudis <davidedefeudis@gmail.com>
2021-07-13 20:47:41 +0200

    Extract text content

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,6 +6,7 @@ import Footer from "../Footer/Footer";
 import Background from "../Background/Background";
 import classes from "./Home.module.css";
 import profilePic from "../../images/kenan_profile_300.jpg";
+import textContent from "./HomeTextContent";
 
 /**
  * The home page. Displays a background image, an image and text.
@@ -20,14 +21,7 @@ export default function Home() {
             <div data-test="profile-container" className={classes.profileContainer}>
                 <img data-test="profile-pic" className={classes.profilePic} src={profilePic} alt="Kenan" />
                 <p data-test="about-text" className={classes.aboutText}>
-                    Kenan grew up in Passau, Germany and graduated from Artez, School of Dance, Arnhem with a BA of performing arts and
-                    completed the SEAD postgraduate program Bodhi Project directed by Susan Quinn where he toured works by Jozef Frucek &
-                    Linda Kapetanea, Martin Nachbar and Robert Clark in Europe. As freelance dancer Kenan worked with Johannes Wieland,
-                    Marion Sparber, Ivan Perez, Unterwegstheater and Anton Lachky. From 2015 till 2017 he joined the dance company of
-                    Stadttheater Bielefeld where he danced in works by Wim Vandekeybus, Simone Sandroni and Sharon Fridman. Kenan is guest
-                    teacher at Die Etage, Berlin, Stadttheater Bielefeld, Marameo Berlin, Staatstheater Braunschweig, TRAK Dance Ensemble,
-                    Tanzbüro Basel and Profitraining Leipzig. As a choreographer Kenan created the piece „Astral“ which was premiered in
-                    Sofia, Bulgaria with further shows in Dock11 Berlin and Arnhem, Netherlands.
+                    {textContent.paragraphs[0]}
                 </p>
             </div>
             <Footer data-test="footer" />
