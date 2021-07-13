d324536795e929e64004d932d922649e293f8b60
DavideDeFeudis <davidedefeudis@gmail.com>
2021-07-13 18:35:10 +0200

    Remove video pages

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import { Route, Switch } from "react-router-dom";
 import Home from "./components/Home/Home";
 import Admin from "./components/Admin/Admin";
 // import About from "./components/About/About";
-// import VideoAstral from "./components/VideoAstral/VideoAstral";
-// import VideoSunson from "./components/VideoSunson/VideoSunson";
 import Workshops from "./components/Workshops/Workshops";
 import Contact from "./components/Contact/Contact";
 // import PrivateRoute from "./PrivateRoute";
@@ -18,8 +16,6 @@ function App() {
                 <Route exact path="/admin" component={Admin} />
                 <Route exact path="/login" component={Login} />
                 {/* <Route path="/about" component={About} /> */}
-                {/* <Route path="/astral" component={VideoAstral} /> */}
-                {/* <Route path="/sunson" component={VideoSunson} /> */}
                 <Route path="/workshops" component={Workshops} />
                 <Route path="/contact" component={Contact} />
                 <Route component={Home} />
